a4f7a337feefd308106dc1cfb79bfab3bfeb2787
vishal arya <vishalarya1701@gmail.com>
2023-04-07 17:10:18 +0530

    ProducerConsumerProblemDemo

---
+++ src/multithreading/ProducerConsumer/MyQueue.java
@@ -0,0 +1,55 @@
+package multithreading.ProducerConsumer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyQueue {
+
+    private int capacity;
+    private Queue<Integer> queue;
+
+    public MyQueue(int capacity){
+        queue = new LinkedList<>();
+        capacity = capacity;
+    }
+
+    public boolean add(int item){
+        synchronized (queue){
+
+            while (queue.size() == capacity){
+                try {
+                    queue.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            queue.add(item);
+            queue.notifyAll(); // notify all the thread that are in the wait state
+            return true;
+
+        }
+
+    }
+    public int remove(){
+
+        synchronized (queue){
+
+            while (queue.size() == 0){
+                // thread has to wait till any other thread add element to the queue
+                try {
+                    queue.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+            }
+            int ele = queue.poll();
+            queue.notifyAll();
+            return ele;
+
+        }
+
+    }
+
+}
---
+++ src/multithreading/ProducerConsumer/ProducerConsumerProblemDemo.java
@@ -0,0 +1,26 @@
+package multithreading.ProducerConsumer;
+
+public class ProducerConsumerProblemDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Main starting");
+
+        Thread thread1 = new Thread(()->{
+            try{
+                Thread.sleep(1);
+                for (int i = 1000; i>0; i--);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        },"States");
+
+        thread1.start();
+
+        while (true){
+            Thread.State state = thread1.getState();
+            System.out.println(state);
+            if (state == Thread.State.TERMINATED) break;
+        }
+
+    }
+}
